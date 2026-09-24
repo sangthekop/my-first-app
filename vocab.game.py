bf46dd5c08f8d997a80793db35a499a065d67618
sangthekop <78627230+sangthekop@users.noreply.github.com>
2026-09-24 16:19:04 +0700

    vocab.game.py

--- vocab.game.py
+++ vocab.game.py
@@ -8,12 +8,18 @@ if "ans1_val" not in st.session_state:
     st.session_state.ans1_val = ""
 if "ans2_val" not in st.session_state:
     st.session_state.ans2_val = ""
+if "ans1_val" not in st.session_state:
+    st.session_state.ans3_val = ""
+if "ans2_val" not in st.session_state:
+    st.session_state.ans4_val = ""
 
 
 # 📌 ฟังก์ชันเคลียร์ค่าเมื่อกดปุ่มเริ่มใหม่
 def reset_game():
     st.session_state.ans1_val = ""  # เคลียร์ค่าช่องข้อ 1
     st.session_state.ans2_val = ""  # เคลียร์ค่าช่องข้อ 2
+    st.session_state.ans3_val = ""  # เคลียร์ค่าช่องข้อ 3
+    st.session_state.ans4_val = ""  # เคลียร์ค่าช่องข้อ 4
     st.session_state.start = time.time()  # เริ่มเวลาใหม่
     st.session_state.is_ended = False  # ปิด Dialog
 
@@ -26,8 +32,8 @@ def show_result_dialog(ans1, ans2):
     st.balloons()
     score = 0
 
-    u_ans1 = ans1.strip().lower()
-    u_ans2 = ans2.strip().lower()
+    u_ans1 = ans3.strip().lower()
+    u_ans2 = ans4.strip().lower()
 
     # ตรวจข้อ 1
     if u_ans1 == "apple":
@@ -44,10 +50,21 @@ def show_result_dialog(ans1, ans2):
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
 
     # ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มตรวจข้อ 3, 4 ตรงนี้
+    
+    if u_ans3 == "Balloon":
+        st.success("✅ ข้อ 3: ถูกต้อง")
+        score += 1
+    else:
 
+    if u_ans3 == "Gift":
+        st.success("✅ ข้อ 4: ถูกต้อง")
+        score += 1
+    else:
+        
+        st.error(f"❌ ข้อ 1: ยังไม่ถูกต้อง (คุณตอบ '{u_ans1}')")
     st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
 
-    if score == 2:
+    if score == 4:
         st.success("🎉 You win!")
     else:
         st.error("💀 You lose!")
@@ -83,9 +100,20 @@ ans2 = st.text_input(
 # อัปเดตค่าล่าสุดเข้าตัวแปร
 st.session_state.ans1_val = ans1
 st.session_state.ans2_val = ans2
+st.session_state.ans3_val = ans3
+st.session_state.ans4_val = ans4
 
 # ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มข้อ 3, 4 ตรงนี้
 
+ans3 = st.text_input(
+    "ข้อ 3: An `B a l l _ _ n `. 🎈",
+    value=st.session_state.ans1_val,
+)
+
+ans4 = st.text_input(
+    "ข้อ 4: Cats love to eat `G _ f t`. 🎁",
+    value=st.session_state.ans2_val,
+)
 
 # 4. ปุ่มส่งคำตอบ
 if "start" in st.session_state and not st.session_state.get("is_ended", False):
@@ -98,7 +126,7 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
 
 # 5. แสดง Dialog ผลลัพธ์
 if st.session_state.get("is_ended", False):
-    show_result_dialog(ans1, ans2)
+    show_result_dialog(ans1, ans2 , ans3 , ans4)
 
 st.divider()
 st.write("นายมนัสวิน พัฒน์ธนานนท์ เลขที่ 18 ม.4/12")
